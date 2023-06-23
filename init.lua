4af35e328d533808dde058399d22f517966d49a5
tane-git <tanelives@gmail.com>
2023-06-24 11:05:49 +1200

    format init.lua

--- init.lua
+++ init.lua
@@ -2,55 +2,55 @@
 vim.g.mapleader = " "
 
 -- <LazyNeoVim>
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not vim.loop.fs_stat(lazypath) then
-	  vim.fn.system({
-	    "git",
-	    "clone",
-	    "--filter=blob:none",
-	    "https://github.com/folke/lazy.nvim.git",
-	    "--branch=stable", -- latest stable release
-	    lazypath,
-	  })
-	end
-	vim.opt.rtp:prepend(lazypath)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
 -- </LazyNeoVim>
 
 
 local plugins = {
-	{
-	  "nvim-tree/nvim-tree.lua",
-	  version = "*",
-	  dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	  },
-	  config = function()
-	    require("nvim-tree").setup {
-		renderer = {
-			icons = {
-				  show = {
-					    file = false,
-					    folder = false,
-					    folder_arrow = true,
-					    git = true,
-					    modified = true,
-				  }
-			}
-		}
-	    }
-	  end,
-	},
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {
+                renderer = {
+                    icons = {
+                        show = {
+                            file = false,
+                            folder = false,
+                            folder_arrow = true,
+                            git = true,
+                            modified = true,
+                        }
+                    }
+                }
+            }
+        end,
+    },
 
-        {
-		    "williamboman/mason.nvim",
-		    build = ":MasonUpdate" -- :MasonUpdate updates registry contents
-	},
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate" -- :MasonUpdate updates registry contents
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate"
-	}
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    }
 }
 
 require("lazy").setup(plugins, opts)
@@ -60,17 +60,17 @@ require("mason").setup()
 
 -- <KeyBindings>
 vim.api.nvim_set_keymap('n', '<leader>sf', ':Ex<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>st', ':NvimTreeToggle<CR>', {noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>sT', ':NvimTreeFindFile<CR>', {noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>st', ':NvimTreeToggle<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>sT', ':NvimTreeFindFile<CR>', { noremap = true })
 -- </KeyBindings>
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    vim.keymap.set('n', 'gh', vim.lsp.buf.hover, { buffer = args.buf })
-  end,
+    callback = function(args)
+        vim.keymap.set('n', 'gh', vim.lsp.buf.hover, { buffer = args.buf })
+    end,
 })
 
 -- From ThePrimeagen:
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
