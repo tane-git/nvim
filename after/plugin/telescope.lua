4803ba4e652f7c306ba5f5dcb051a3e1a5c917da
tane-git <tanelives@gmail.com>
2023-07-07 18:02:07 +1200

    add colorscheme

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
-end)
+snd)
 -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -6,8 +6,14 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.2',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function() vim.cmd('colorscheme rose-pine') end
+  })
 end)
