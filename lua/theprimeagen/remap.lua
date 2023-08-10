d647e31c1ca1f5abe28fdc567f828a06c806a74b
tane-git <tanelives@gmail.com>
2023-08-10 18:09:37 +1200

    refactor remap keymaps and bring in some stuff from ideavim

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -1,42 +1,69 @@
+-- ~~ GENERAL ~~
 vim.g.mapleader = " "
+
+-- dont press Q?
+vim.keymap.set("n", "Q", "<nop>")
+
+
+-- ~~ IDE NAVIGATION ~~
+
 vim.keymap.set("n", "<leader>sl", vim.cmd.Ex)
 
+
+-- ~~ FILE NAVIGATION ~~
+
+-- Scroll half page up/down remap from <C-u/d> to <C-j/k> (and add zz to centre on scroll)
+vim.keymap.set('n', '<C-j>', '<C-d>zz', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-j>', '<C-d>zz', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', '<C-u>zz', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-k>', '<C-u>zz', { noremap = true, silent = true })
+
+-- (Already implemented above)
+-- WHEN scrolling THEN cursor stays in middle
+-- vim.keymap.set("n", "<C-j>", "<C-j>zz")
+-- vim.keymap.set("n", "<C-k>", "<C-k>zz")
+
+-- Swap { and } (which are under the j/k keys on a symbols layer)
+vim.keymap.set({'n', 'v'}, '{', '}', { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '}', '{', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-u>', '<C-o>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-i>', '<C-i>', { noremap = true, silent = true })
+
+
+-- ~~ TEXT EDITING ~~
+
+-- Move with selected text
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- WHEN appending lines THEN cursor stays in one place
 vim.keymap.set("n", "J", "mzJ`z")
 
--- WHEN scrolling THEN cursor stays in middle
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
--- WHEN searching THEN cursor stays in the middle
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- EXAMPLE
--- vim.keymap.set("n", "<leader>vwm", function()
---  require("vim-with-me").StartVimWithMe()
--- end)
-
--- greatest remap ever
+-- Use void register
 vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- dont press Q?
-vim.keymap.set("n", "Q", "<nop>")
+-- ~~ SEARCHING ~~
 
--- TMUX
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- WHEN searching THEN cursor stays in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
--- FORMATTING
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- ~~ RUN SHIT ~~
+vim.keymap.set("n", "<leader>rf", vim.lsp.buf.format)
+
+
+-- ~~ EXAMPLES ~~
+
+-- vim.keymap.set("n", "<leader>vwm", function()
+--  require("vim-with-me").StartVimWithMe()
+-- end)
 
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -45,3 +72,7 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- TMUX
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
