25d8d518d9f0d220736b018bece5c603e5cc5276
t.wilson <t.wilson@rocketlab.co.nz>
2024-08-15 08:52:55 +1200

    Format

--- init.lua
+++ init.lua
@@ -1,39 +1,39 @@
-require("set") -- local file
-require("remap") -- local file
+require("set")             -- local file
+require("remap")           -- local file
 require("window_commands") -- local file
 require("buffer_commands") -- local file
 
 -- bootstrap Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
     -- { "miikanissi/modus-themes.nvim", priority = 1000 },
-    { "rose-pine/neovim", name = "rose-pine" },
+    { "rose-pine/neovim",                name = "rose-pine" },
 
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
     "nvim-tree/nvim-tree.lua",
 
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
 
     -- LSP
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
 
     {
         'nvim-telescope/telescope.nvim',
@@ -43,7 +43,7 @@ local plugins = {
 
     "tpope/vim-fugitive",
 
-    "nvim-tree/nvim-tree.lua"
+    "nvim-tree/nvim-tree.lua",
 }
 
 require("lazy").setup(plugins)
@@ -51,17 +51,17 @@ require("lazy").setup(plugins)
 -- vim.cmd([[colorscheme modus]])
 
 -- Copy and pasted from github/tree-sitter
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust" },
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  highlight = {
-    enable = true,
-  },
+    highlight = {
+        enable = true,
+    },
 }
